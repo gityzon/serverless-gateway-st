22c213c831d803145ecfb41fb5eb09c64344b95d
gityzon <109445070+gityzon@users.noreply.github.com>
2023-01-06 15:28:55 +0800

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+
+#download gateway-st lastest
+curl -L https://github.com/storj/gateway-st/releases/latest/download/gateway_linux_amd64.zip -o /home/gateway_linux_amd64.zip
+cd /home
+unzip gateway_linux_amd64.zip
+rm -f gateway_linux_amd64.zip
+chmod 777 /home/gateway
+
+#start gateway
+/home/gateway run --access $access --minio.access-key $accesskey --minio.secret-key $secretkey --server.address 0.0.0.0:3000 --website
