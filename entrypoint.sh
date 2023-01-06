e221c95971d5174e56a2c09444b5ab5b9c2d9087
gityzon <109445070+gityzon@users.noreply.github.com>
2023-01-06 15:49:18 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
+#!/bin/sh
 
 #download gateway-st lastest
-curl -L https://github.com/storj/gateway-st/releases/latest/download/gateway_linux_amd64.zip -o /home/gateway_linux_amd64.zip
-cd /home
-unzip gateway_linux_amd64.zip
-rm -f gateway_linux_amd64.zip
-chmod 777 /home/gateway
+	curl -L https://github.com/storj/gateway-st/releases/latest/download/gateway_linux_amd64.zip -o /home/gateway_linux_amd64.zip
+	cd /home
+	unzip gateway_linux_amd64.zip
+	rm -f gateway_linux_amd64.zip
+	chmod 777 /home/gateway
 
-#start gateway
+#run gateway
 /home/gateway run --access $access --minio.access-key $accesskey --minio.secret-key $secretkey --server.address 0.0.0.0:3000 --website
