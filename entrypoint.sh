fc8d7a57579cf56985c86dfeda523f94da3abfa6
gityzon <109445070+gityzon@users.noreply.github.com>
2023-01-07 17:56:56 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,4 @@
 	chmod 777 /home/gateway
 
 #run gateway
-/home/gateway run --access 1cCYAHUHkDDVrwGZpPDxP8VrzBmLsF8uQwNzNW3zjbSf3bCzTSaZZRtXsS3RUgJnfo1CCPyPa6U6Jf6oKY8vAv11mNpMe7mSJFnTD9kZsisHS29XS3yZ3rcxDFt3coSSw515JGVG6uU4qEpDFcmwdv214rLn6zW8HXfiK7ZjLVPJjQAbrJ2vQQE6b5paZwaLKhpFdyxFeQjMefwRmJSywu9JcMZpCEoH26XtfGawimCtpbzzXadK4dvmbv3jpLfaG4TGuoCX1NEz --minio.access-key $accesskey --minio.secret-key $secretkey --server.address 0.0.0.0:3000 --website
+/home/gateway run --access $access --minio.access-key $accesskey --minio.secret-key $secretkey --server.address 0.0.0.0:3000 --website
